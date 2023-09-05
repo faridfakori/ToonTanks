1a72ca5cc7e3a3dee472f151d4f32bcb924c41b0
Mohammadfarid fakori <fakori.farid@gmail.com>
2023-09-04 23:16:35 -0400

    Making the code Better

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -5,7 +5,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Tank.h"
 #include "Tower.h"
-
+#include "ToonTanksPlayerController.h"
 
 void AToonTanksGameMode::ActorDied(AActor* DeadActor)
 {
@@ -13,10 +13,9 @@ void AToonTanksGameMode::ActorDied(AActor* DeadActor)
    {
         Tank->HandleDestruction();
 
-        if (Tank->GetTankPlayerController())
+        if (ToonTankPlayerController)
         {
-              Tank->DisableInput(Tank->GetTankPlayerController());
-              Tank->GetTankPlayerController()->bShowMouseCursor = false;
+              ToonTankPlayerController->SetPlayerEnabledState(false);
         }
    }
     else if (ATower* DestroyedTower = Cast<ATower>(DeadActor))    
@@ -32,4 +31,6 @@ void AToonTanksGameMode::BeginPlay()
     Super::BeginPlay();
     
     Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this,0));
+
+    ToonTankPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this,0));
 }
